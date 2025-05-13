66717eba284c7a8f7e4ff4501a4b3f2e9c6048e6
master-dhanesh <dhanesh1296@gmail.com>
2025-05-13 21:14:50 +0530

    Two Way Binding Form Elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,59 @@
 import { useState } from "react";
-import Create from "./components/Create";
-import Read from "./components/Read";
+
 const App = () => {
-    const [users, setusers] = useState([
-        { name: "John", age: 12 },
-        { name: "Ankur", age: 32 },
-        { name: "Ayush", age: 22 },
+    const [todos, settodos] = useState([
+        { id: 1, title: "Kamm krle bhai", isCompleted: false },
     ]);
+
+    const [title, settitle] = useState("");
+    const [completed, setcompleted] = useState(false);
+    const [gender, setgender] = useState("female");
+    const [city, setcity] = useState("delhi");
+
     return (
         <div>
-            <Create />
-            <Read users={users} setusers={setusers} />
-            {/* Read(users,setusers) */}
+            <h1>Create Tasks</h1>
+            <form>
+                <input
+                    onChange={(e) => settitle(e.target.value)}
+                    value={title}
+                    type="text"
+                    placeholder="title"
+                />
+                <br />
+                <br />
+                <input
+                    checked={completed}
+                    onChange={(e) => setcompleted(e.target.checked)}
+                    type="checkbox"
+                />
+                Completed
+                <br /> <br />
+                <input
+                    value="male"
+                    onChange={(e) => setgender(e.target.value)}
+                    checked={gender == "male" && true}
+                    type="radio"
+                />
+                male
+                <input
+                    value="female"
+                    onChange={(e) => setgender(e.target.value)}
+                    checked={gender == "female" && true}
+                    type="radio"
+                />
+                Female
+                <br />
+                <br />
+                <select value={city} onChange={(e) => setcity(e.target.value)}>
+                    <option value="delhi">Delhi</option>
+                    <option value="mumbai">Mumbai</option>
+                    <option value="kolkata">Kolkata</option>
+                </select>
+                <br />
+                <br />
+                <button>Create Todo</button>
+            </form>
         </div>
     );
 };
